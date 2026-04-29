df2184d4e22ead63f05c35db075faf19ef8170e4
Alex'terieur <alexterieurjardinetcreation@gmail.com>
2026-04-29 22:32:51 +0200

    Update PWA icons with Alex'terieur logo

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,11 @@
-const CACHE = 'tarifs-v17';
+const CACHE = 'tarifs-v18';
 const ASSETS = [
   './',
   './grille-tarifaire.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
+  './apple-touch-icon.png',
   './logo.png'
 ];
 
